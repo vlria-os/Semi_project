81632d9ebf90f7343f7cd8db844379e85f56be31
vlria <shslvllria540@gmail.com>
2026-01-12 16:56:05 +0900

    outbound

--- src/main/java/com/example/demo/controller/outbound_req_Controller.java
+++ src/main/java/com/example/demo/controller/outbound_req_Controller.java
@@ -1,10 +1,8 @@
 package com.example.demo.controller;
 
-import com.example.demo.dto.Inbound_reqDto;
-import com.example.demo.dto.Inbound_reqListDto;
-import com.example.demo.dto.Outbound_reqDto;
-import com.example.demo.dto.Outbound_reqListDto;
+import com.example.demo.dto.*;
 import com.example.demo.service.OutboundService;
+import com.example.demo.service.ProductStockService;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -12,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -19,11 +18,18 @@ import java.util.List;
 @RequiredArgsConstructor
 public class outbound_req_Controller {
     private final OutboundService outboundService;
+    private final ProductStockService service;
 
-    @GetMapping("/office_staff/outbound_req")
-    public String outbound_reqForm(){
-        //재고 현황 불러오기
-        return "office_staff/outbound_req";
+    @GetMapping("/content/outbound_req")
+    public String outbound_reqForm(@RequestParam(required = false)String keyword,
+                                   @RequestParam(required = false)String status,
+                                   Model model){
+        List<ProductStockDto> p = service.selectAll(keyword, status);
+        model.addAttribute("p", p);
+
+        model.addAttribute("navFragment", "fragment/nav/officeNav");
+        model.addAttribute("content", "content/outbound_req");
+        return "layout";
     }
 
     @PostMapping("/office_staff/outbound_req")
