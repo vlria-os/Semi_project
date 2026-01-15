25eecc513c180145db189f20a1c0a4ed9c95fec6
Parse-Edward <epislotus@gmail.com>
2026-01-15 21:15:26 +0900

    fixed productList with rough edges left

--- src/main/java/com/example/demo/service/ProductService.java
+++ src/main/java/com/example/demo/service/ProductService.java
@@ -3,6 +3,7 @@ package com.example.demo.service;
 import com.example.demo.dto.CategoryDto;
 import com.example.demo.dto.ProductDto;
 import com.example.demo.dto.Product_imageDto;
+import com.example.demo.dto.SearchDto;
 import com.example.demo.mapper.ImageMapper;
 import com.example.demo.mapper.ProductMapper;
 import com.example.demo.pagination.PageInfo;
@@ -10,36 +11,36 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class ProductService {
     private final ProductMapper productMapper;
     private final ImageMapper imageMapper;
+    private final String UPLOADPATH = "c:/image_Semi/";
 
-    public Map<String,Object> productList(int pageNum){
-        String keyword="";
-
+    public Map<String,Object> productList(int pageNum, SearchDto searchDto){
         Map<String,Object> map=new HashMap<>();
-        map.put("pageNum",pageNum);
-        int totalRowCount=productMapper.count(keyword);
+        map.put("searchDto", searchDto);
 
+        int totalRowCount=productMapper.count(map);
         PageInfo pageInfo=new PageInfo(pageNum,10,5,totalRowCount);
 
         map.put("startRow",pageInfo.getStartRow());
         map.put("endRow",pageInfo.getEndRow());
-        map.put("keyword",keyword);
 
-        List<ProductDto> productDtos=productMapper.selectAll_keyword(map);
-        for(ProductDto p:productDtos){
-            String category=p.getCategory();
-            p.setCategory(category);
-            p.setSave_name(imageMapper.select(p.getProduct_id()));
-        }
+        List<ProductDto> productDtos=productMapper.selectAllKeyword(map);
+//        for(ProductDto p:productDtos){
+//            p.setSave_name(imageMapper.select(p.getProduct_id()));
+//        }
 
         Map<String,Object> result=new HashMap<>();
         result.put("productDtos",productDtos);
@@ -47,31 +48,70 @@ public class ProductService {
         return result;
     }
 
-    public Map<String,Object> productList(int pageNum, String keyword){
-        Map<String,Object> map=new HashMap<>();
-        map.put("pageNum",pageNum);
+    @Transactional
+    public int updateProduct(ProductDto productDto){
+        MultipartFile file= productDto.getFile();
 
-        int totalRowCount=productMapper.count(keyword);
+        if(file != null && !file.isEmpty()){
+            String orgFileName= file.getOriginalFilename();
+            String extName= orgFileName.substring(orgFileName.lastIndexOf("."));
+            String saveFileName= UUID.randomUUID() + extName;
 
-        PageInfo pageInfo=new PageInfo(pageNum,10,5,totalRowCount);
+            try{
+                file.transferTo(new File(UPLOADPATH + saveFileName));
+                Product_imageDto imageDto= new Product_imageDto(0, UPLOADPATH + saveFileName, saveFileName);
+                productMapper.insert_image(imageDto);
 
-        map.put("startRow",pageInfo.getStartRow());
-        map.put("endRow",pageInfo.getEndRow());
-        map.put("keyword",keyword);
-
-        List<ProductDto> productDtos=productMapper.selectAll_keyword(map);
-
-        for(ProductDto p:productDtos){
-            String category=p.getCategory();
-            p.setCategory(category);
-            p.setSave_name(imageMapper.select(p.getProduct_id()));
+                productDto.setImage_id(imageDto.getImage_id());
+            }catch (IOException e){
+                e.printStackTrace();
+            }
         }
-
-        Map<String,Object> result=new HashMap<>();
-        result.put("productDtos",productDtos);
-        result.put("pageInfo",pageInfo);
-        return result;
+        return productMapper.updateProduct(productDto);
     }
+
+    public ProductDto getProductById(int productId){
+        ProductDto dto= productMapper.selectOneProduct(productId);
+        if(dto != null){
+            dto.setSave_name(imageMapper.select(dto.getProduct_id()));
+        }
+        return dto;
+    }
+
+    @Transactional
+    public void deleteProduct(int productId){
+        String saveName=productMapper.select_product(productId);
+        File file= new File(UPLOADPATH + saveName);
+        if(file.exists()) file.delete(); //파일삭제
+
+        productMapper.deleteProduct(productId);
+    }
+
+//    public Map<String,Object> productList(int pageNum, String keyword){
+//        Map<String,Object> map=new HashMap<>();
+//        map.put("pageNum",pageNum);
+//
+//        int totalRowCount=productMapper.count(keyword);
+//
+//        PageInfo pageInfo=new PageInfo(pageNum,10,5,totalRowCount);
+//
+//        map.put("startRow",pageInfo.getStartRow());
+//        map.put("endRow",pageInfo.getEndRow());
+//        map.put("keyword",keyword);
+//
+//        List<ProductDto> productDtos=productMapper.selectAll_keyword(map);
+//
+//        for(ProductDto p:productDtos){
+//            String category=p.getCategory();
+//            p.setCategory(category);
+//            p.setSave_name(imageMapper.select(p.getProduct_id()));
+//        }
+//
+//        Map<String,Object> result=new HashMap<>();
+//        result.put("productDtos",productDtos);
+//        result.put("pageInfo",pageInfo);
+//        return result;
+//    }
      @Transactional
     public int insert(ProductDto productDto,
                       Product_imageDto product_imageDto){
