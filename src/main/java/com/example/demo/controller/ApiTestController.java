4f7ec8a5a8135a0ab25aeb2c25a875d8a0694bf0
novolunteeer <novolunteeer@gmail.com>
2026-01-16 16:46:19 +0900

    api 출력 성공

--- src/main/java/com/example/demo/controller/ApiTestController.java
+++ src/main/java/com/example/demo/controller/ApiTestController.java
@@ -17,7 +17,7 @@ public class ApiTestController {
     @ResponseBody
     public String getCulture(){
         String url="https://apis.data.go.kr/6450000/JeonbukPrincipalProductService/getJeonbukPrincipalProduct" +
-                "?ServiceKey=14828702d32a2c63fa843d3cdab151ae271fa46bd7f7284daebd0555ab349fd2&Category=B&Area=01";
+                "?ServiceKey=내_키&Category=B&Area=01";
         /*
         RestTemplate: Spring에서 제공하는 HTTP 통신 도구
         GET / POST / PUT / DELETE 등 REST API 호출 가능
