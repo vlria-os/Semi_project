06563fddc4422f4bf991b1a6f82ec0d24fc02bf1
vlria <shslvllria540@gmail.com>
2026-01-20 15:20:05 +0900

    엑셀 다운로드

--- src/main/java/com/example/demo/controller/ExcelController.java
+++ src/main/java/com/example/demo/controller/ExcelController.java
@@ -1,7 +1,11 @@
 package com.example.demo.controller;
 
 import com.alibaba.excel.EasyExcel;
+import com.alibaba.excel.ExcelWriter;
+import com.alibaba.excel.write.metadata.WriteSheet;
 import com.example.demo.dto.ApprovalDto;
+import com.example.demo.dto.ProgressInDto;
+import com.example.demo.dto.ProgressOutDto;
 import com.example.demo.service.ExcelService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -14,6 +18,7 @@ import java.io.IOException;
 import java.net.URLEncoder;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @RestController
@@ -23,16 +28,25 @@ public class ExcelController {
 
     @GetMapping("/download")
     public void download(HttpServletResponse response) throws IOException {
-        String date=LocalDateTime.now().toString();
+        String date = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss"));
         String fileName = URLEncoder.encode("approval_list"+date+".xlsx", "UTF-8").replaceAll("\\+", "%20");
         response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
         response.setCharacterEncoding("utf-8");
         response.setHeader("Content-Disposition", "attachment; filename=" + fileName);
 
-        List<ApprovalDto> approvalDtos=excelService.download();
+        List<ProgressInDto> list=excelService.download();
 
-        EasyExcel.write(response.getOutputStream(), ApprovalDto.class)
-                .sheet("Approval List")
-                .doWrite(approvalDtos);
+        List<ProgressOutDto> list_out=excelService.download_out();
+
+        try(ExcelWriter excelWriter = EasyExcel.write(response.getOutputStream()).build()){
+            // 첫 번째 시트
+            WriteSheet sheetIn = EasyExcel.writerSheet(0, "Inbound List").head(ProgressInDto.class).build();
+            excelWriter.write(list, sheetIn);
+
+            // 두 번째 시트
+            WriteSheet sheetOut = EasyExcel.writerSheet(1, "Outbound List").head(ProgressOutDto.class).build();
+            excelWriter.write(list_out, sheetOut);
+        }
+
     }
 }
--- src/main/java/com/example/demo/service/ExcelService.java
+++ src/main/java/com/example/demo/service/ExcelService.java
@@ -1,19 +1,38 @@
 package com.example.demo.service;
 
 import com.example.demo.dto.ApprovalDto;
+import com.example.demo.dto.ProgressInDto;
+import com.example.demo.dto.ProgressOutDto;
 import com.example.demo.mapper.ApprovalMapper;
+import com.example.demo.mapper.ProgressMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 public class ExcelService {
     private final ApprovalMapper approvalMapper;
+    private final ProgressMapper progressMapper;
 
-    public List<ApprovalDto> download(){
-        List<ApprovalDto> approvalDtos=approvalMapper.selectAll();
-        return approvalDtos;
+    public List<ProgressInDto> download(){
+        Map<String,Object> map=new HashMap<>();
+        map.put("startRow",1);
+        map.put("endRow",progressMapper.count_in(""));
+        map.put("keyword", "");
+
+        return progressMapper.select_in(map);
+    }
+
+    public List<ProgressOutDto> download_out(){
+        Map<String,Object> map=new HashMap<>();
+        map.put("startRow",1);
+        map.put("endRow",progressMapper.count_out(""));
+        map.put("keyword", "");
+
+        return progressMapper.select_out(map);
     }
 }
