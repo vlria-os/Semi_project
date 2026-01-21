2583fdd04a0fd88c1662cd87aac510295571a434
vlria <shslvllria540@gmail.com>
2026-01-21 10:48:10 +0900

    로그인 인터셉트 추가

--- src/main/java/com/example/demo/controller/log/LoginController.java
+++ src/main/java/com/example/demo/controller/log/LoginController.java
@@ -33,6 +33,8 @@ public class LoginController {
                 model.addAttribute("error", "비밀번호가 틀렸습니다!");
                 return "login/form";
             } else {
+                session.setAttribute("lastUserActionTime", System.currentTimeMillis());
+
                 if (userDto.getRole_id() == 1) {
                     session.setAttribute("webuser_id", userDto.getWebuser_id());
                     session.setAttribute("role_id", userDto.getRole_id());
